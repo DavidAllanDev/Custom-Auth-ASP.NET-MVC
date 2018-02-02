4b597c56d39a6657fccc1325e9557ce338b17506
David Allan Ribeiro <profissional@david-allan.com.br>
2018-02-02 00:13:00 -0200

    Added Account/Models classes

---
+++ Custom-Auth/Models/AccountModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Custom_Auth.Web.Models
+{
+    public class AccountModel
+    {
+        private List<Account> _accounts;
+
+        public AccountModel()
+        {
+            _accounts = new List<Account>() { };
+            _accounts.Add(
+                            new Account() {
+                                            UserName = "User",
+                                            Password = "xXx",
+                                            Roles = new string[] {"root","sudo","sa", "user"}
+                                          }
+                         );
+            _accounts.Add(
+                            new Account()
+                            {
+                                UserName = "Root",
+                                Password = "xXx",
+                                Roles = new string[] { "root", "sudo", "sa" }
+                            }
+                         );
+        }
+
+        public Account FindByUserName(string userName)
+        {
+            return _accounts.Single(a => a.UserName.Equals(userName));
+        }
+
+        public bool IsAuthenticable(string userName, string password)
+        {
+            return _accounts.Find(A => A.UserName.Equals(userName) && A.Password.Equals(password)) != null;
+        }
+    }
+}
---
+++ Custom-Auth/ViewModels/AccountViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Custom_Auth.Web.Models;
+
+namespace Custom_Auth.Web.ViewModels
+{
+    public class AccountViewModel
+    {
+        public Account Account { get; set; }
+    }
+}
