6676d1b2e8db3d8cdfe62e3f77dc6380641ac4e2
David Allan Ribeiro <profissional@david-allan.com.br>
2018-02-02 00:14:09 -0200

    Added Login Action to AccountControler

--- Custom-Auth/Controllers/AccountController.cs
+++ Custom-Auth/Controllers/AccountController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Custom_Auth.Web.Models;
+using Custom_Auth.Web.ViewModels;
 
 namespace Custom_Auth.Web.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Account
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AccountViewModel avm)
+        {
+            Account account = avm.Account;
+
+            if(String.IsNullOrEmpty(account.UserName) || String.IsNullOrEmpty(account.Password))
+            {
+                ViewBag.Error = "User or Password invalid";
+                return View("Index");
+            }
+
+            AccountModel accountM = new AccountModel();
+
+            if (!accountM.IsAuthenticable(account.UserName, account.Password))
+            {
+                ViewBag.Error = "User or Password invalid²";
+                return View("Index");
+            }
+
+            return View("Success");
         }
     }
 }
