6ec2ebbf2f14000d252eac71f38a38c695597fd5
David Allan Ribeiro <profissional@david-allan.com.br>
2018-02-02 02:09:12 -0200

    Created Demo Controller and Views

---
+++ Custom-Auth/Controllers/DemoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Custom_Auth.Web.Security;
+using Custom_Auth.Web.Models;
+
+namespace Custom_Auth.Web.Controllers
+{
+    public class DemoController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [CustomAuthorize(Roles = "root")]
+        public ActionResult Work1()
+        {
+            return View("Work1");
+        }
+
+        [CustomAuthorize(Roles = "root,sudo")]
+        public ActionResult Work2()
+        {
+            return View("Work2");
+        }
+
+        [CustomAuthorize(Roles = "root,sudo,sa")]
+        public ActionResult Work3()
+        {
+            return View("Work3");
+        }
+    }
+}
