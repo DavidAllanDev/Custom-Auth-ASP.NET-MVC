acbec6fde26c71655fd2eceb0aa95630e1109057
David Allan Ribeiro <profissional@david-allan.com.br>
2018-02-02 02:01:29 -0200

    Created a Custom implementation of IPrincipal

---
+++ Custom-Auth/Security/CustomPrincipal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using Custom_Auth.Web.Models;
+
+namespace Custom_Auth.Web.Security
+{
+    public class CustomPrincipal : IPrincipal
+    {
+        private Account _account;
+
+
+        public CustomPrincipal(Account account)
+        {
+            _account = account;
+
+            Identity = new GenericIdentity(account.UserName);
+        }
+
+        public IIdentity Identity{ get; set; }
+
+        public bool IsInRole(string role)
+        {
+            var roles = role.Split(new char[] { ',' });
+            return roles.Any(r => _account.Roles.Contains(r));
+        }
+    }
+}
