25e5a761622ec1d23dc1972b50f9d1dc5ec62c53
David Allan Ribeiro <profissional@david-allan.com.br>
2018-02-06 11:53:47 -0200

    Created Custom-Auth.Interface to add IAccount interface

---
+++ Custom-Auth.Interfaces/ModelInterface/IAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace Custom_Auth.Interfaces.ModelInterface
+{
+    public interface IAccount
+    {
+        string Password { get; set; }
+        string[] Roles { get; set; }
+        string UserName { get; set; }
+    }
+}
+
---
+++ Custom-Auth.Interfaces/ModelInterface/IAccountModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Custom_Auth.Interfaces.ModelInterface
+{
+    public interface IAccountModel
+    {
+        IAccount FindByUserName(string userName);
+        bool IsAuthenticable(string userName, string password);
+    }
+}
