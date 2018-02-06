8f6c6ce7407ed179c9b0f0e8a4a8b1c70d24eb9e
David Allan Ribeiro <profissional@david-allan.com.br>
2018-02-06 11:59:30 -0200

    Added Repository projetc to provide sample data for the solution

---
+++ Custom-Auth.Repository/Accounts.cs
@@ -0,0 +1,37 @@
+﻿using Custom_Auth.Domain.Model;
+using Custom_Auth.Interfaces.ModelInterface;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Custom_Auth.Repository
+{
+    public static class Accounts
+    {
+        private static List<IAccount> _accounts = new List<IAccount>()
+        {
+            new Account {
+                    UserName = "User",
+                    Password = "xXx",
+                    Roles = new string[] { "sa", "user" }
+              },
+            new Account()
+              {
+                  UserName = "sasa",
+                  Password = "xXx",
+                  Roles = new string[] { "sudo", "sa" }
+              },
+            new Account()
+              {
+                  UserName = "Root",
+                  Password = "xXx",
+                  Roles = new string[] { "root", "sudo", "sa" }
+              }
+        };                  
+
+        public static List<IAccount> AccountList(string userName) => _accounts;
+
+        public static IAccount FindByUserName(string userName) => _accounts.Single(a => a.UserName.Equals(userName));
+
+        public static bool IsAuthenticable(string userName, string password) => _accounts.Find(A => A.UserName.Equals(userName) && A.Password.Equals(password)) != null;
+    }
+}
