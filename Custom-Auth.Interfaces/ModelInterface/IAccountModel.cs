d14b33b5d54499e62f97c2281f4b5add3131174b
David Allan Ribeiro <profissional@david-allan.com.br>
2018-02-07 00:13:59 -0200

    Removed the IAccountModel interface, not used anymore

--- Custom-Auth.Interfaces/ModelInterface/IAccountModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Custom_Auth.Interfaces.ModelInterface
-{
-    public interface IAccountModel
-    {
-        IAccount FindByUserName(string userName);
-        bool IsAuthenticable(string userName, string password);
-    }
-}
